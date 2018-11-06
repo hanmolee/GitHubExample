d8a3d4001a31a000326592e49d4fa6c8668570aa
hanmolee <dlgksah@gmail.com>
2018-11-06 10:27:58 +0900

    BaseActivity 구현 Activity Inject 재사용

---
+++ app/src/main/java/com/leehanmo/githubexample/base/BaseActivity.kt
@@ -0,0 +1,14 @@
+package com.leehanmo.githubexample.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import dagger.android.AndroidInjection
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+    }
+
+}
